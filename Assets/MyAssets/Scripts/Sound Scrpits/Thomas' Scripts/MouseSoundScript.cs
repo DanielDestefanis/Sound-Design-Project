caf77b29fb9ed46ce8ea1c22973109109561a3d1
Thomas Greene <d4c213@gmail.com>
2021-07-20 11:43:24 +1000

    Final Push for Thomas
    Finalized the rat and spider ambient sounds

---
+++ Assets/MyAssets/Scripts/Sound Scrpits/Thomas' Scripts/MouseSoundScript.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MouseSoundScript : MonoBehaviour
+{
+    public AudioSource MouseSound;
+
+    public void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            MouseSound.Play();
+        }
+    }
+    public void OnTriggerExit(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            MouseSound.Stop();
+        }
+    }
+}
+       
---
+++ Assets/MyAssets/Scripts/Sound Scrpits/Thomas' Scripts/SpiderSoundScript.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpiderSoundScript : MonoBehaviour
+{
+    public AudioSource SpiderSound;
+
+    public void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            SpiderSound.Play();
+        }
+    }
+    public void OnTriggerExit(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            SpiderSound.Stop();
+        }
+        
+    }
+}
